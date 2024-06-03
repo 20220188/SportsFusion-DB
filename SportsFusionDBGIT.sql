c22943e37518c4e0734527a3dafdf29619f7dc75
Kevin Vladimir Rodríguez Alvarado <133680440+KevinVRA@users.noreply.github.com>
2024-06-03 07:42:53 -0600

    Update SportsFusionDBGIT.sql

--- SportsFusionDBGIT.sql
+++ SportsFusionDBGIT.sql
@@ -5,7 +5,7 @@ USE sportfusionDB;
 
 CREATE TABLE tb_deportes (
 id_deporte INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-nombre_deporte VARCHAR(25),
+nombre_deporte VARCHAR(25) unique,
 estado_retro TINYINT(1),
 imagen_deporte VARCHAR(25)
 );
@@ -20,18 +20,18 @@ VALUES('Futbol', 1, 'default.png'),
 CREATE TABLE tb_clientes (
 id_cliente INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 nombre_cliente VARCHAR(50),
-telefono_cliente VARCHAR(9),
-correo_cliente VARCHAR(100),
+telefono_cliente VARCHAR(9) unique,
+correo_cliente VARCHAR(100) unique,
 dirección_cliente VARCHAR(250),
-alias_cliente VARCHAR(50),
+alias_cliente VARCHAR(50) unique,
 clave_cliente VARCHAR(64),
-estado_cliente boolean
+estado_cliente BOOLEAN DEFAULT(1)
 );
 
-INSERT INTO tb_clientes(nombre_cliente, telefono_cliente,correo_cliente, dirección_cliente, alias_cliente, clave_cliente,estado_cliente)
-VALUES('Kevin', '7795-9878', 'kevin10@gmail.com', 'La ibertad, El Salvador', 'kevin1234', '12345678',1),
-('Dominic', '7788-3452', 'dominic10@gmail.com', 'San salvador, El Salvador', 'dominic1234', '12345678',1),
-('Jafet', '7657-2323', 'jafetM10@gmail.com', 'San salvador, El Salvador', 'jafetM1234', '12345678',1);
+INSERT INTO tb_clientes(nombre_cliente, telefono_cliente,correo_cliente, dirección_cliente, alias_cliente, clave_cliente)
+VALUES('Kevin', '7795-9878', 'kevin10@gmail.com', 'La ibertad, El Salvador', 'kevin1234', '12345678'),
+('Dominic', '7788-3452', 'dominic10@gmail.com', 'San salvador, El Salvador', 'dominic1234', '12345678'),
+('Jafet', '7657-2323', 'jafetM10@gmail.com', 'San salvador, El Salvador', 'jafetM1234', '12345678');
 
 SELECT * FROM tb_clientes;
 
@@ -39,20 +39,17 @@ CREATE TABLE tb_administradores(
 id_admin INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 nombre_admin VARCHAR(50),
 apellido_admin VARCHAR(50),
-correo_admin VARCHAR(100),
-alias_admin VARCHAR(50),
+correo_admin VARCHAR(100) unique,
+alias_admin VARCHAR(50) unique,
 clave_admin  VARCHAR(64)
 );
 
-INSERT INTO tb_administradores(nombre_admin, apellido_admin, correo_admin, alias_admin, clave_admin)
-VALUES('Kevin','Rodriguez','kevin1@gmail.com','kevinAdmin','12345678'),
-('Jafet', 'Melara', 'jafet1@gmail.com', 'jafetAdmin', '12345678');
 
 SELECT * FROM tb_administradores;
 
 CREATE TABLE tb_tallas(
 id_talla INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-talla VARCHAR(10)
+talla VARCHAR(10) unique
 );
 
 INSERT INTO tb_tallas(talla)
@@ -62,7 +59,7 @@ VALUES('M'), ('S'), ('L');
 /*Tpo de producto es para definir si son coleccionable o de actualidad*/
 CREATE TABLE tb_categorias(
 id_categoria INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-nombre_categoria VARCHAR(30),
+nombre_categoria VARCHAR(30) unique,
 imagen_categoria varchar(25),
 descripcion_categoria VARCHAR(200)
 );
@@ -77,7 +74,7 @@ VALUES('Coleccionable', 'default.png', 'Articulos deportivos coleccionables'),
 /*Categorias es para definir si son camisetas, medias, snekers, etc*/
 CREATE TABLE tb_tipo_productos(
 id_tipo_producto INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-tipo_producto VARCHAR(25)
+tipo_producto VARCHAR(25) unique
 );
 
 INSERT INTO tb_tipo_productos(tipo_producto)
@@ -98,7 +95,7 @@ VALUES('Producto de muy buena calidad',5),
 
 CREATE TABLE tb_generos(
 id_genero INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-genero VARCHAR(10)
+genero VARCHAR(10) unique
 );
 
 INSERT INTO tb_generos(genero)
@@ -128,6 +125,9 @@ insert INTO tb_productos(nombre_producto, descripcion, imagen, id_categoria, id_
 VALUES('Jersey FC Barcelona', 'Jersey del mejor equipo', 'default.png',2,1,1),
 ('Jersey Detroit Pistons 2005', 'Jersey de campeones', 'default.png',1,1,2),
 ('Camiseta de voleybol', 'Camiseta deportiva', 'default.png',2,1,3);
+
+SELECT * FROM tb_productos;
+
 
 
 /* detalle productos lleva precio, stock y llave foranea de tallas*/
@@ -180,13 +180,12 @@ estado_valoracion BOOLEAN
 );
 
 INSERT INTO tb_valoraciones_productos(id_opinion,id_detalle_producto, id_cliente,estado_valoracion)
-VALUES(1,1,2,1), (2,2,1,1),(3,3,3,1);
+VALUES(1,1,2,1), (2,2,1,1),(3,3,3,1),(1,3,2,1);
 
 
 
-
-
-
+        
+        
 /*
 CREATE TABLE tb_productos_deportes(
 id_proDep INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -203,7 +202,7 @@ REFERENCES tb_deportes (id_deporte)
 
 CREATE TABLE tb_estado_pedidos(
 id_estado_pedido INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-estado_pedido VARCHAR(15)
+estado_pedido VARCHAR(15) unique
 );
 
 INSERT INTO tb_estado_pedidos(estado_pedido)
@@ -213,17 +212,21 @@ VALUES('Pendiente'),('Finalizado'),('Cancelado');
 CREATE TABLE tb_pedidos (
 id_pedido INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 direccion_pedido VARCHAR(250),
-fecha_registro DATE NOT NULL DEFAULT current_timestamp(),
+fecha_registro DATETIME NOT NULL DEFAULT current_timestamp(),
 id_cliente INT,
 CONSTRAINT FK_pedido_cliente
 FOREIGN KEY(id_cliente)
-REFERENCES tb_clientes (id_cliente)
+REFERENCES tb_clientes (id_cliente),
+id_estado_pedido INT,
+CONSTRAINT FK_estado_pedido
+FOREIGN KEY(id_estado_pedido)
+REFERENCES tb_estado_pedidos (id_estado_pedido)
 );
 
-INSERT INTO tb_pedidos(direccion_pedido, id_cliente)
-VALUES('San Salvador, El Salvador',2),
-('La Libertad',1),
-('San Salvador',3);
+INSERT INTO tb_pedidos(direccion_pedido, id_cliente,id_estado_pedido)
+VALUES('San Salvador, El Salvador',2,1),
+('La Libertad',1,1),
+('San Salvador',3,1);
 
 SELECT * FROM tb_clientes;
 
@@ -238,15 +241,11 @@ REFERENCES tb_pedidos (id_pedido),
 id_producto INT, 
 CONSTRAINT FK_detalle_producto
 FOREIGN KEY(id_producto)
-REFERENCES tb_productos (id_producto),
-id_estado_pedido INT,
-CONSTRAINT FK_estado_pedido
-FOREIGN KEY(id_estado_pedido)
-REFERENCES tb_estado_pedidos (id_estado_pedido)
+REFERENCES tb_productos (id_producto)
 );
 
-INSERT INTO tb_detalle_pedidos(cantidad_pedido,precio_pedido,id_pedido,id_producto,id_estado_pedido)
-VALUES(5,150,1,1,2), (5,225,2,2,2),(5,100,3,3,2);
+INSERT INTO tb_detalle_pedidos(cantidad_pedido,precio_pedido,id_pedido,id_producto)
+VALUES(5,150,1,1), (5,225,2,2),(5,100,3,3);
 
 
 
